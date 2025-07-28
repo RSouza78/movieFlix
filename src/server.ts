1b48cfb57a5d901926b1057a985269ee02685c6b
RSouza78 <rodrigoacaciovs@gmail.com>
2025-07-28 08:20:49 -0300

    Adicionada a mensagem de erro ao tentar adicionar o filme

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movies.findMany({
         orderBy: {
@@ -16,6 +18,26 @@ app.get('/movies', async (_, res) => {
         }
     });
     res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movies.create({
+            data: {
+                title: title,
+                genre_id: genre_id,
+                language_id: language_id,
+                oscar_count: oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+
+        res.status(201).send('Filme criado com sucesso');
+    } catch (error) {
+        return res.status(500).send({ message: 'Erro ao criar o filme' });
+    }
 });
 
 app.listen(port, () => {
